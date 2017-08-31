e7a62386c5a6de8b36db8d480b18a57f6748bc38
m1koo <136057505@qq.com>
2017-08-31 10:38:38 +0800

    dd

--- app/src/main/java/com/zd/miko/riji/MVP/Service/SynService.java
+++ app/src/main/java/com/zd/miko/riji/MVP/Service/SynService.java
@@ -129,8 +129,6 @@ public class SynService extends IntentService {
             @Override
             public void onSuccess(String response) {
                 Log.i("xyz",response);
-
-
             }
 
             @Override
