924c2665a55b680b8fa100a822514cbbd1c47cde
m1koo <136057505@qq.com>
2017-10-11 21:33:56 +0800

    viewPager的版本

--- app/src/main/java/com/zd/miko/riji/MVP/MainContainerActivity/MainActivity.java
+++ app/src/main/java/com/zd/miko/riji/MVP/MainContainerActivity/MainActivity.java
@@ -48,7 +48,6 @@ public class MainActivity extends AppCompatActivity {
 
 
     private Snackbar snackbar;
-    //testfadfadfadf
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
